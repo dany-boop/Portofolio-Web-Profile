e122816d0075220d0a2379de168041494d5ac91e
dany-boop <dny77853@gmail.com>
2023-11-02 14:50:00 +0700

    minor

--- src/data/projectsData.ts
+++ src/data/projectsData.ts
@@ -1,12 +1,12 @@
 export const projectsData = [
-    {
-        ProjectName : "Drew Laundry",
-        Time: "25/01/20223",
-        Deskprisi : "Project UKL Laundry SMK Telkom"
-    }
-    {
-        ProjectName : "Dreww Laundry",
-        Time: "25/01/20223",
-        Deskprisi : "Project UKL Laundry SMK Telkom"
-    }
-]
+  {
+    ProjectName: 'Drew Laundry',
+    Time: '25/01/20223',
+    Deskprisi: 'Project UKL Laundry SMK Telkom',
+  },
+  {
+    ProjectName: 'Dreww Laundry',
+    Time: '25/01/20223',
+    Deskprisi: 'Project UKL Laundry SMK Telkom',
+  },
+];
